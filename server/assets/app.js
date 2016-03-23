a126aa3cba602256d85369696b419ab1e8bfa6bc
markwillis75 <markwillis75>
2016-03-23 17:25:57 +0000

    Breaking angular into services
    Remove $http from the angular controller by creating a service which
    uses the $http and injecting the service into the controller

--- server/assets/app.js
+++ server/assets/app.js
@@ -1,20 +1,32 @@
 var app=angular.module('app', [])
 
-app.controller('PostsCtrl', function($scope, $http){
+app.service('PostsSvc', function($http){
+    this.fetch = function(){
+        return $http.get('/api/posts')
+    }
+
+    this.create = function(post){
+        return $http.post('/api/posts', post)
+    }
+})
+
+app.controller('PostsCtrl', function($scope, PostsSvc){
 
     $scope.addPost = function(){
-    	$http.post('/api/posts', {
-    		username: 'dickeyxxx',
-    		body: $scope.postBody
-    	})
-    	.success(function(post){
-    		$scope.posts.unshift(post)
-    		$scope.postBody = null
-    	})
+        if ($scope.postBody){
+            PostsSvc.create({
+                username: 'dickeyxxx',
+                body: $scope.postBody
+            })
+            .success(function(post){
+            $scope.posts.unshift(post)
+            $scope.postBody = null
+        })
+        }
     }
 
 	// starting data
-	$http.get('/api/posts')
+	PostsSvc.fetch()
 	.success(function(posts){
 		$scope.posts=posts
 	})
