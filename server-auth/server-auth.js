3f6f8166871637ac9b877a3f849a432a2230d081
markwillis75 <markwillis75>
2016-03-24 23:26:38 +0000

    Authentication with MongoDB
    - Installed mongoose
    - New /models/user.js with username & password
    - server-auth.js:
    - deleted lodash, local array and find/validate user functions
    - new POST '/user' route to create users
    - '/session' route locates user in database and hashes request password
    against hashed password from db
    - GET '/user' route returns user from database by decoding jwt token

--- server-auth/server-auth.js
+++ server-auth/server-auth.js
@@ -1,47 +1,63 @@
 var express = require('express')
 var jwt     = require('jwt-simple')
-var lodash  = require('lodash')
 var bcrypt  = require('bcryptjs') // using bcryptjs instead of bcrypt as the latter has python and openssl dependencies on Windows
+var User    = require('./models/user')
 
 var app     = express()
 
 app.use(require('body-parser').json())
 
-/* The password (literal string "pass" was hashed outside of the app using bcryptjs.
-   In reality it wouls be hashed on-the-fly inside the app */
-var users     = [{username: 'dickeyxxx', password: '$2a$10$ShG7medas2RMOOYIo0pqDe0cQTuG1p6LPvrskZryarX/dQ2vJooSS'}]
 var secretKey = 'supersecretkey'
 
-function findUserByUsername(username){
-	return lodash.find(users, {username:username})
-}
-
-function validateUser(user, password, callback){
-	bcrypt.compare(password, user.password, callback)
-}
-
-app.post('/session', function(req, res){
-	var user = findUserByUsername(req.body.username)
-
-    // use an asynchronous callback to prevent bcrypt blocking the thread
-	validateUser(user, req.body.password, function(err, valid){
-		if (err || !valid){
-			return res.send(401) //not authorized
-		}
-
-		var token = jwt.encode({username: user.username}, secretKey)
-	    res.json(token)
-
+// creating a new user
+app.post('/user', function(req, res, next){
+	var user = new User({username: req.body.username})
+	bcrypt.hash(req.body.password, 10, function(err, hash){
+		user.password = hash
+		user.save(function(err, user){
+			if (err){
+				throw next(err)
+			}
+			res.sendStatus(201)
+		})
 	})
 })
 
+// logging a user in, return jwt token or 401
+app.post('/session', function(req, res, next){
+	User.findOne({username: req.body.username}, function(err, user){
+		if (err){
+			return next(err)
+		}
+
+		if (!user){
+			return res.sendStatus(401)
+		}
+
+		// use an asynchronous callback to prevent bcrypt blocking the thread
+	    bcrypt.compare(req.body.password, user.password, function(err, valid){
+		    if (err){
+		    	return next (err)
+		    }
+
+		    if(!valid){
+			    return res.sendStatus(401) //not authorized
+		    }
+
+		    var token = jwt.encode({username: user.username}, secretKey)
+	        res.json(token)
+	    })
+	})
+})
+
+// retrieve user for a given jwt token
 app.get('/user', function(req, res){
 	var token = req.headers['x-auth']
-	var user  = jwt.decode(token, secretKey)
+	var auth  = jwt.decode(token, secretKey)
 
-    //TODO pull user from database
-
-    res.json(user)
+    User.findOne({username: auth.username}, function(err, user){
+    	res.json(user)
+    })
 })
 
 app.listen(3000)
