e68a3a8f4d11d8067a716e5bf8496e27be5f7e38
markwillis75 <markwillis75>
2016-03-24 22:34:19 +0000

    Using bcrypt (bcryptjs)
    The book recommends use of bcrypt, but that has dependencies on
    python/openssl/others on Windows.  bcryptjs has no such dependencies, so
    used that instead even though it's slower than bcrypt.
    
    The username/password are still stored in a local array instead of Mongo
    - that will be next on the list.  The password (literal string "pass")
    was hashed outside of the app using bcryptjs and is stored in the array
    in hashed form.  It will be hashed on-the-fly inside the app when the
    login functionality is implemented

--- server-auth/server-auth.js
+++ server-auth/server-auth.js
@@ -1,31 +1,38 @@
 var express = require('express')
 var jwt     = require('jwt-simple')
 var lodash  = require('lodash')
+var bcrypt  = require('bcryptjs') // using bcryptjs instead of bcrypt as the latter has python and openssl dependencies on Windows
 
 var app     = express()
 
 app.use(require('body-parser').json())
 
-var users     = [{username: 'dickeyxxx', password:'pass'}]
+/* The password (literal string "pass" was hashed outside of the app using bcryptjs.
+   In reality it wouls be hashed on-the-fly inside the app */
+var users     = [{username: 'dickeyxxx', password: '$2a$10$ShG7medas2RMOOYIo0pqDe0cQTuG1p6LPvrskZryarX/dQ2vJooSS'}]
 var secretKey = 'supersecretkey'
 
 function findUserByUsername(username){
 	return lodash.find(users, {username:username})
 }
 
-function validateUser(user, password){
-	return user.password === password
+function validateUser(user, password, callback){
+	bcrypt.compare(password, user.password, callback)
 }
 
 app.post('/session', function(req, res){
 	var user = findUserByUsername(req.body.username)
 
-	if (!validateUser(user, req.body.password)){
-		return res.sendStatus(401) //unauthorized
-	}
+    // use an asynchronous callback to prevent bcrypt blocking the thread
+	validateUser(user, req.body.password, function(err, valid){
+		if (err || !valid){
+			return res.send(401) //not authorized
+		}
 
-	var token = jwt.encode({username: user.username}, secretKey)
-	res.json(token)
+		var token = jwt.encode({username: user.username}, secretKey)
+	    res.json(token)
+
+	})
 })
 
 app.get('/user', function(req, res){
