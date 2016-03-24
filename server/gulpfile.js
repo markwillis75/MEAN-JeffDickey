ddc64f33ae30c591a9a355bcb29b7e0a68e1fde1
markwillis75 <markwillis75>
2016-03-24 12:16:46 +0000

    Building CSS with Gulp
    - Install gulp-stylus
    - New /server/css/app.styl file
    - New /server/gulp folder containing css.js, which uses stylus to
    process /css/styl files and compiles /assets/app.cs
    - Break gulpfile.js 'js' task out to /server/gulp/scripts.js
    - New feature in gulpfile.js to scoop up all scripts in server/gulp
    folder
    - update /layouts/posts.html to use app.css

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -3,16 +3,13 @@ var concat     = require('gulp-concat')
 var uglify     = require('gulp-uglify')
 var annotate   = require('gulp-ng-annotate')
 var sourcemaps = require('gulp-sourcemaps')
+var fs         = require('fs')
 
-gulp.task('js', function(){
-	gulp.src(['ng/module.js', 'ng/**/*.js'])
-	    .pipe(sourcemaps.init())
-	      .pipe(concat('app.js'))
-	      .pipe(annotate())
-	      .pipe(uglify())
-	    .pipe(sourcemaps.write())
-	    .pipe(gulp.dest('assets'))
-})
+// read all the files under /gulp
+fs.readdirSync(__dirname + '/gulp')
+    .forEach(function(task){
+    	require('./gulp/' + task)
+    })
 
 gulp.task('watch:js', ['js'], function(){
 	gulp.watch('ng/**/*.js', ['js'])
