cf3af1cc7fa8b5fb53f7878079ff2c1977d08985
markwillis75 <markwillis75>
2016-03-24 00:37:28 +0000

    Uglify
    Install gulp-uglify
    Install gulp-ng-annotate
    Update gulpfile.js to annotate then uglify the concatenated
    /assets/app.js file

--- server/gulpfile.js
+++ server/gulpfile.js
@@ -1,8 +1,12 @@
-var gulp   = require('gulp')
-var concat = require('gulp-concat')
+var gulp     = require('gulp')
+var concat   = require('gulp-concat')
+var uglify   = require('gulp-uglify')
+var annotate = require('gulp-ng-annotate')
 
 gulp.task('js', function(){
 	gulp.src(['ng/module.js', 'ng/**/*.js'])
 	    .pipe(concat('app.js'))
+	    .pipe(annotate())
+	    .pipe(uglify())
 	    .pipe(gulp.dest('assets'))
 })
